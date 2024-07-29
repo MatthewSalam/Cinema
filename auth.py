81e3f942033a10b81a0631c172268bd7673dddf7
MatthewSalam <salammatthew7@gmail.com>
2024-07-29 10:46:20 +0100

    hashed at last

--- auth.py
+++ auth.py
@@ -1,21 +1,27 @@
 import mysql.connector
 import operations
+import hashlib
 
 #Allowing the initial userid to be zero
 user_id = 0
 con  = mysql.connector.connect(host = "localhost", user = "root", password = "", database = "cinema_db")
 cursor = con.cursor()
 
+def hash_password(password):
+   return hashlib.sha256(password.encode()).hexdigest()
+
 
 def insert_details(user_name, email, password):
     #Appending thhe details into the database
-    sql = (f"INSERT INTO `users`(`user_name`, `email`, `password`) VALUES ('{user_name}','{email}','{password}')")
+    hashed_password =hash_password(password)
+    sql = (f"INSERT INTO `users`(`user_name`, `email`, `password`) VALUES ('{user_name}','{email}','{hashed_password}')")
     cursor.execute(sql)
     con.commit()
     print("User details inputted")
     return
 
 def validate_user_details(username,password):
+    hashed_password =hash_password(password)
     #Making sure the user details inputted match the ones in the DB
     sql = (f"SELECT user_name,password FROM `users` WHERE `user_name` = '{username}'")
     cursor.execute(sql)
@@ -23,7 +29,8 @@ def validate_user_details(username,password):
     result = cursor.fetchone()
     # return True
 
-    if username != result[0] or password != result[1] :
+
+    if username is None or result[1] != hashed_password :
         #Making sure the user inputs correct username or password
        print("Invalid username or password")
        login()
@@ -56,7 +63,7 @@ def movie_status():
       print("Movie ticket is available")
     else:
       print("Sorry, movie is not available")
-    user_recognition = input("Are you a registered user:  ")
+    user_recognition = input("Are you a registered user:(yes/no)  ")
     if user_recognition == "yes":
       print("Login")
       login()
